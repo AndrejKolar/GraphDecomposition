c74e96fb1d5d66779b8266223037dbeaa249cabd
Andrej Kolar <andrej.kolar@me.com>
2012-03-14 13:33:28 +0100

    Added GraphDecompostion.Utils project. Added method for checking if a graph can be decomposed. Started work on stinsons algorithm.

--- GraphDecomposition.Algorithms/Stinson.cs
+++ GraphDecomposition.Algorithms/Stinson.cs
@@ -5,7 +5,27 @@ using System.Text;
 
 namespace GraphDecomposition.Algorithms
 {
-    class Stinson
+    public class Stinson
     {
+        /// <summary>
+        /// Number of vertices in a graph
+        /// </summary>
+        int v;
+
+        /// <summary>
+        /// Number of triples in a STS
+        /// </summary>
+        int b;
+
+        public void StartAlgorithm(int v)
+        {
+            this.v = v;
+            this.b = v * (v - 1) / 6;
+        }
+
+        private void ConstructBlocks()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- GraphDecomposition.Tester/Program.cs
+++ GraphDecomposition.Tester/Program.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using GraphDecomposition.Algorithms;
+using GraphDecomposition.Utils;
+
 namespace GraphDecomposition.Tester
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Stinson aStinson = new Stinson();
+
+            int numVertex = 9;
         }
     }
 }
