be88a2aa6d1a27f6415702ba5453342819044955
Andrej Kolar <andrej.kolar@me.com>
2012-03-25 17:28:38 +0200

    Removed unused usings.

--- GraphDecomposition.Tester/Program.cs
+++ GraphDecomposition.Tester/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using GraphDecomposition.Algorithms;
-using GraphDecomposition.Utils;
 using GraphDecomposition.GraphElements;
 
 namespace GraphDecomposition.Tester
