488c3f56698863abedb14098132d0e1be0938f74
Andrej Kolar <andrej.kolar@me.com>
2012-03-25 17:36:33 +0200

    Added method for deciding which construction can be used.

--- GraphDecomposition.Utils/GraphUtils.cs
+++ GraphDecomposition.Utils/GraphUtils.cs
@@ -12,5 +12,34 @@ namespace GraphDecomposition.Utils
         {
             return (numVertex % 6 == 1) || (numVertex % 6 == 3); 
         }
+
+        /// <summary>
+        /// Chooses construction type based on the number of vertices in the graph
+        /// </summary>
+        /// <param name="numVertex"></param>
+        /// <returns></returns>
+        public static ConstructionType ChooseConstruction(int numVertex)
+        {
+            if (numVertex % 6 == 1)
+            {
+                return ConstructionType.Skolem;
+            }
+            else if (numVertex % 6 == 3)
+            {
+                return ConstructionType.Bose;
+            }
+            else
+            {
+                return ConstructionType.None;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Enum defines which construction can be used
+    /// </summary>
+    public enum ConstructionType
+    {
+        Bose, Skolem, None
     }
 }
