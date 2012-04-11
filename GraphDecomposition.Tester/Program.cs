19dabfc79c92ffe979778e8e23c103b7b2bb4bea
Andrej Kolar <andrej.kolar@me.com>
2012-04-11 16:56:54 +0200

    Tester console project changed to use StinsonContinous.cs algorithm.

--- GraphDecomposition.Tester/Program.cs
+++ GraphDecomposition.Tester/Program.cs
@@ -11,11 +11,26 @@ namespace GraphDecomposition.Tester
             Stinson aStinson = new Stinson();
             Skolem aSkolem = new Skolem();
             Bose aBose = new Bose();
+            StinsonContinous aStinsonContinous = new StinsonContinous();
 
-            int numVertex = 61;
+            int numVertex = 31;
 
-            SteinerTripleSystem sts = aStinson.StartAlgorithm(numVertex);
+            SteinerTripleSystem sts = aStinsonContinous.StartAlgorithm(numVertex);
 
+            writeDecomposition(sts);
+
+            for (int i = 0; i < 5; i++)
+            {
+                sts = aStinsonContinous.NextDecomposition(sts);
+
+                writeDecomposition(sts);
+            }
+
+            
+        }
+
+        private static void writeDecomposition(SteinerTripleSystem sts)
+        {
             foreach (Triple t in sts)
             {
                 Console.Write("|");
@@ -25,7 +40,7 @@ namespace GraphDecomposition.Tester
                 Console.WriteLine("|");
             }
 
-            
+            Console.WriteLine("------------------");
         }
     }
 }
