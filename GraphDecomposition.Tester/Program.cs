b5b2215be6e9b6802fc73c9bf9f7b28714507df3
Andrej Kolar <andrej.kolar@me.com>
2012-04-11 21:18:22 +0200

    Refactored the Tester console project.

--- GraphDecomposition.Tester/Program.cs
+++ GraphDecomposition.Tester/Program.cs
@@ -6,26 +6,26 @@ namespace GraphDecomposition.Tester
 {
     class Program
     {
+        private static int NUM_VERTEX = 13;
+        private static int ITERATIONS_COUNT = 5;
+
         static void Main(string[] args)
         {
             Stinson aStinson = new Stinson();
             Skolem aSkolem = new Skolem();
             Bose aBose = new Bose();
-            StinsonContinous aStinsonContinous = new StinsonContinous();
+            StinsonExtended aStinsonContinous = new StinsonExtended();
 
-            int numVertex = 31;
-
-            SteinerTripleSystem sts = aStinsonContinous.StartAlgorithm(numVertex);
+            SteinerTripleSystem sts = aStinsonContinous.StartAlgorithm(NUM_VERTEX);
 
             writeDecomposition(sts);
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < ITERATIONS_COUNT; i++)
             {
                 sts = aStinsonContinous.NextDecomposition(sts);
 
                 writeDecomposition(sts);
             }
-
             
         }
 
