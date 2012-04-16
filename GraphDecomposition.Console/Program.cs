3120cee2714a4321a6b7b22530a72c66a9f7d6b3
Andrej Kolar <andrej.kolar@me.com>
2012-04-16 20:24:48 +0200

    Refactored and added documentation for the console project.

--- GraphDecomposition.Console/Program.cs
+++ GraphDecomposition.Console/Program.cs
@@ -7,31 +7,45 @@ namespace GraphDecomposition.Tester
 {
     class Program
     {
+        /// <summary>
+        /// Number of vertices in the graph
+        /// </summary>
         private static int NUM_VERTEX = 13;
+
+        /// <summary>
+        /// Number of iterations for the StinsonExtended algorithm
+        /// </summary>
         private static int ITERATIONS_COUNT = 5;
+
+        /// <summary>
+        /// Path of the logfile for the generated incidence matrices
+        /// </summary>
         private static string LOG_FILE_PATH = "test_log.txt";
 
+        /// <summary>
+        /// Main function of the console program
+        /// </summary>
+        /// <param name="args">Command line arguments not used</param>
         static void Main(string[] args)
         {
-            Stinson aStinson = new Stinson();
-            Skolem aSkolem = new Skolem();
-            Bose aBose = new Bose();
-            StinsonExtended aStinsonContinous = new StinsonExtended();
+            StinsonExtended aStinsonExtended = new StinsonExtended();
             LogUtils.CreateLogFile(LOG_FILE_PATH);
 
-            SteinerTripleSystem sts = aStinsonContinous.StartAlgorithm(NUM_VERTEX);
-            
+            SteinerTripleSystem sts = aStinsonExtended.StartAlgorithm(NUM_VERTEX);            
             LogUtils.AppendIncidenceMatrix(sts);
 
             for (int i = 0; i < ITERATIONS_COUNT; i++)
             {
-                sts = aStinsonContinous.NextDecomposition(sts);
-
+                sts = aStinsonExtended.NextDecomposition(sts);
                 LogUtils.AppendIncidenceMatrix(sts);
             }
             
         }
 
+        /// <summary>
+        /// Writes the tripple to the console
+        /// </summary>
+        /// <param name="sts">Triple for console output</param>
         private static void writeDecomposition(SteinerTripleSystem sts)
         {
             foreach (Triple t in sts)
