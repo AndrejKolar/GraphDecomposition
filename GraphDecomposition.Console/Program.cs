6847c2f6981efaf176bfe0deeb5ac1fcc344dd8e
Andrej Kolar <andrej.kolar@me.com>
2012-04-17 20:04:54 +0200

    Added iteration counter. Removed unnecessary methods.

--- GraphDecomposition.Console/Program.cs
+++ GraphDecomposition.Console/Program.cs
@@ -10,12 +10,12 @@ namespace GraphDecomposition.Tester
         /// <summary>
         /// Number of vertices in the graph
         /// </summary>
-        private static int NUM_VERTEX = 7;
+        private static int NUM_VERTEX = 13;
 
         /// <summary>
         /// Number of iterations for the StinsonExtended algorithm
         /// </summary>
-        private static int ITERATIONS_COUNT = 5;
+        private static int ITERATIONS_COUNT = 500;
 
         /// <summary>
         /// Path of the logfile for the generated incidence matrices
@@ -28,56 +28,22 @@ namespace GraphDecomposition.Tester
         /// <param name="args">Command line arguments not used</param>
         static void Main(string[] args)
         {
-            StinsonExtended aStinsonExtended = new StinsonExtended();
-            
+            StinsonExtended aStinsonExtended = new StinsonExtended();         
 
             SteinerTripleSystem sts = aStinsonExtended.StartAlgorithm(NUM_VERTEX);
             LogUtils.CreateLogFile(LOG_FILE_PATH, sts);
 
-
             for (int i = 1; i < ITERATIONS_COUNT; i++)
-            {
+            {                
                 sts = aStinsonExtended.NextDecomposition(sts);
                 LogUtils.AppendIncidenceMatrix(sts);
+
+                Console.WriteLine("Iteration {0} / {1}", i.ToString(), ITERATIONS_COUNT.ToString());
             }
 
-            writeDecompositionInfo(sts);
-            
+            Console.WriteLine("Iteration {0} / {1}", ITERATIONS_COUNT.ToString(), ITERATIONS_COUNT.ToString());
+            Console.WriteLine("Decomposition done.");
         }
 
-        /// <summary>
-        /// Writes info about the decomposition to the console
-        /// </summary>
-        /// <param name="sts">STS(v)</param>
-        private static void writeDecompositionInfo(SteinerTripleSystem sts)
-        {
-            Console.WriteLine("v: {0} b: {1}", NUM_VERTEX, sts.NumTriples());
-            Console.WriteLine("Decomposition finished");
-
-            if (ITERATIONS_COUNT != 0)
-            {
-                Console.WriteLine("Number of iterations: {0}", ITERATIONS_COUNT);
-                Console.WriteLine("Log file locaton: " + LOG_FILE_PATH); 
-            } 
-
-        }
-
-        /// <summary>
-        /// Writes the decomposition result to the console
-        /// </summary>
-        /// <param name="sts">STS(v)</param>
-        private static void writeDecompositionResult(SteinerTripleSystem sts)
-        {
-            foreach (Triple t in sts)
-            {
-                Console.Write("|");
-                Console.Write(t.X + " ");
-                Console.Write(t.Y + " ");
-                Console.Write(t.Z);
-                Console.WriteLine("|");
-            }
-
-            Console.WriteLine("------------------");
-        }
     }
 }
